cbda0c7738b45e784938d93c56880cbf28319552
davisjoseph6 <davisjoseph767@gmail.com>
2024-01-13 23:30:25 +0100

    endpoint set-up to accept a file upload

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from main import segment_everything
 
 app = FastAPI()
+
+@app.post("/segment-image")
+async def segment_image(file: UploadFile = File(...))):
+    # Further processing will be done here
+    pass
 
 @app.get("/")
 def read_root():
