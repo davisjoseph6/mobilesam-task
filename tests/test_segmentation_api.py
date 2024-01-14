c0606cdaef6946fd8457e20ad581a5313f1c983e
davisjoseph6 <davisjoseph767@gmail.com>
2024-01-15 00:10:28 +0100

    fixed syntax

--- tests/test_segmentation_api.py
+++ tests/test_segmentation_api.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_segment_image_api(url, image_path):
-    files = {'file': open(image_path, 'rib')}
+    files = {'file': open(image_path, 'rb')}
     response = requests.post(url, files=files)
 
     if response.status_code == 200:
@@ -10,7 +10,7 @@ def test_segment_image_api(url, image_path):
             f.write(response.content)
         print("Segmented image saved as segmented_image.png")
     else:
-        print("Error in API call:", response.status_code, response.txt)
+        print("Error in API call:", response.status_code, response.text)
 
 if __name__ == "__main__":
     api_url = 'http://127.0.0.1:8000/segment-image' # Update with the correct URL if needed
